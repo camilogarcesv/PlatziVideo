8c6d1a5baaa4b61070bbaf69088d4856561af798
Juan Garces <jgarces@sophilabs.com>
2021-08-25 00:40:45 -0500

    Añadiendo Hooks a los componentes, configurando Custom Hooks y PropTypes

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../assets/styles/components/Categories.scss';
 
 const Categories = ({ children, title }) => (
-  <div>
+  <div className='categories'>
     <h3 className='categories__title'>{title}</h3>
     {children}
   </div>
